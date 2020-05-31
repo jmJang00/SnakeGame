a165b21c7f9d9ef8fa8f71eee986593aa89f5f4a
Jaeman Jang <woak1010@kookmin.ac.kr>
2020-05-31 22:32:12 +0900

    add: constant.h

---
+++ constant.h
@@ -0,0 +1,13 @@
+#define EMPTY_SPACE 0
+#define WALL 1
+#define IMMUNE_WALL 2
+#define SNAKE_HEAD 3
+#define SNAKE_BODY 4
+#define GROWTH_ITEM 5
+#define POISON_ITEM 6
+#define GATE 7
+
+#define UP 0
+#define DOWN 1
+#define RIGHT 2
+#define LEFT 3
